9b4f6d6995a24a07df1a6a526f3576d2bd8ebd20
Evan <xonoobify@gmail.com>
2025-07-21 20:56:46 -0700

    Update 7/21/25

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,26 @@
 import { Text, View } from "react-native";
+import { styles } from "./indexstyle";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hi, this is my first app.</Text>
+    <View style={styles.container}>
+      {/* this is for the header */}
+      <View style={styles.headerRow}>
+        <View style={styles.headerBar}>
+          <Text style={styles.headerText}>Hi, this is my first app.</Text>
+        </View>
+        <View style = {styles.profileIcon}></View>
+      </View>
+      {/*This is for content block 1*/}
+      <View>
+        <Text>Content block 1</Text>
+      </View>
+      {/*This is for content block 2*/}
+      <View>
+        <Text>Content block 2</Text>
+      </View>
+
     </View>
+
   );
 }
---
+++ app/indexstyle.ts
@@ -0,0 +1,31 @@
+import { StyleSheet } from "react-native";
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+    },
+    headerRow: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 20,
+    },
+    headerBar: {
+        flex: 1,
+        backgroundColor: '#f0f0f0',
+        padding: 10,
+        borderRadius: 5,
+    },
+    headerText: {
+        textAlign: 'center',
+    },
+    profileIcon: {
+        width:30,
+        height:30,
+        backgroundColor: 'purple',
+        borderRadius: 15,
+        marginLeft: 10,
+    },
+
+
+
+});
